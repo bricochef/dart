783b24b4b99ce5aa85c091f2399f63b612b24cfb
bricochef <78785304+bricochef@users.noreply.github.com>
2024-02-05 18:19:55 +0100

    Versión española

--- bin/dart1.dart
+++ bin/dart1.dart
@@ -4,10 +4,10 @@ void main(List<String> arguments) {
     // Obtener la fecha y hora actual
     DateTime now = DateTime.now();
     // Formatear la fecha y hora
-    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+    String formattedDateTime = "${now.day}-${now.month}-${now.year} ${now.hour}:${now.minute}:${now.second}";
 
     // Imprimir el mensaje con la fecha y hora
     // ignore: prefer_typing_uninitialized_variables
     var dartApplication1;
-    print('Hello world: ${dartApplication1.calculate()}! ($formattedDateTime)');
+    print('Hola Mundo!: ${dartApplication1.calculate()}! ($formattedDateTime)');
 }
